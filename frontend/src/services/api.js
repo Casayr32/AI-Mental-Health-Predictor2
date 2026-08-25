86c4e82114f21ccb1380f12bbc56570d8c5cf51c
Casayr32 <casayrpodcast@gmail.com>
2026-08-25 11:36:47 +0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -6,9 +6,9 @@ import axios from 'axios';
 const API = axios.create({
     baseURL: window.location.hostname === 'localhost' 
         ? 'http://localhost:5000/api' 
-        : 'https://ai-mental-health-predictor2-3.onrender.com/api'
+        : 'https://ai-mental-health-predictor2-k2zl.onrender.com'
 });
-
+//https://ai-mental-health-predictor2-3.onrender.com/api
 // REQUEST INTERCEPTOR
 API.interceptors.request.use(
     (config) => {
